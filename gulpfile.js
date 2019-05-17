b18dd1b7aca018fbab7cc9e8345a954a5004945e
Ryan <dooioomoo.work@gmail.com>
2019-05-17 17:10:50 +0900

    创建RyanBlog模板基础框架

---
+++ gulpfile.js
@@ -0,0 +1,185 @@
+/*template:
+
+npm i -global gulp gulp-sass gulp-concat gulp-autoprefixer gulp-minify browser-sync child_process gulp-cssnano del gulp-rename gulp-eslint gulp-imagemin gulp-newer gulp-postcss webpack webpack-stream --save-dev
+
+
+*/
+"use strict";
+/************************************************************************************/
+// Load Basic plugins
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const concat = require('gulp-concat');
+
+/** == Prefix CSS with Autoprefixer : npm install --save-dev gulp-autoprefixer */
+const autoprefixer = require("autoprefixer");
+/** == A basic use is to watch all CSS files : npm install -g browser-sync */
+const browsersync = require("browser-sync").create();
+/** == sevice for spawn()、exec()、execFile()、fork() */
+const cp = require("child_process");
+/** == Minify CSS with cssnano : npm install gulp-cssnano --save-dev */
+const cssnano = require("cssnano");
+const minify = require('gulp-minify');
+/** == Delete files and folders using globs : npm install --save del */
+const del = require("del");
+/** == gulp-rename is a gulp plugin to rename files easily : npm install gulp-rename --save */
+const rename = require("gulp-rename");
+/** == Minify PNG, JPEG, GIF and SVG images with imagemin : npm install --save-dev gulp-imagemin */
+const imagemin = require("gulp-imagemin");
+/** == Only pass through newer source files : npm install gulp-newer --save-dev */
+const newer = require("gulp-newer");
+/** == Prevent pipe breaking caused by errors from gulp plugins */
+const plumber = require("gulp-plumber");
+/** == PostCSS gulp plugin to pipe CSS through several plugins, but parse CSS only once. : npm install --save-dev gulp-postcss */
+const postcss = require("gulp-postcss");
+/** == webpack is a module bundler. Its main purpose is to bundle JavaScript files for usage in a browser, yet it is also capable of transforming, bundling, or packaging just about any resource or asset.
+ : npm in  stall --save-dev webpack
+ */
+//const webpack = require("webpack");
+//const webpackconfig = require("./webpack.config.js");
+/** == Run webpack as a stream to conveniently integrate with gulp. : npm install --save-dev webpack-stream */
+//const webpackstream = require("webpack-stream");
+
+/************************************************************************************/
+// create variables
+
+var siteDir = './',
+    asseDir = siteDir + 'assets/',
+    imgsDir = asseDir + 'images/',
+    csopDir = asseDir + 'css/',
+    jsopDir = asseDir + 'js/',
+    sassDir = asseDir + 'sass/',
+    ignoreFile = [sassDir + 'custom.scss',sassDir + 'admin.scss'],
+    sassfile = [],
+    ignoreFilePlug = '';
+
+//ignoreFile2 = '!' + sassDir + 'admin.scss';
+
+/************************************************************************************/
+function createignoreFileList(){
+    sassfile = [sassDir + '*.scss'];
+    for (var i in ignoreFile){
+        ignoreFilePlug = '!'+ignoreFile[i];
+        sassfile.push(ignoreFilePlug);
+    }
+    console.log(sassfile);
+}
+// BrowserSync
+function browserSync(done) {
+    browsersync.init({
+        server: {
+            baseDir: siteDir
+        },
+        port: 3000
+    });
+    done();
+}
+
+// BrowserSync Reload
+function browserSyncReload(done) {
+    browsersync.reload();
+    done();
+}
+
+// Clean assets
+function clean() {
+    return del([sassDir +csopDir]);
+}
+
+// Optimize Images
+function images() {
+    return gulp
+        .src(imgsDir + "**/*")
+        .pipe(newer(imgsDir))
+        .pipe(
+            imagemin([
+                imagemin.gifsicle({ interlaced: true }),
+                imagemin.jpegtran({ progressive: true }),
+                imagemin.optipng({ optimizationLevel: 5 }),
+                imagemin.svgo({
+                    plugins: [
+                        {
+                            removeViewBox: false,
+                            collapseGroups: true
+                        }
+                    ]
+                })
+            ])
+        )
+        .pipe(gulp.dest(imgsDir));
+}
+
+// CSS task
+function css() {
+    return gulp
+        .src(sassfile)
+        .pipe(plumber())
+        .pipe(sass({ outputStyle: "expanded" }))
+        .pipe(concat('common.css'))
+        .pipe(gulp.dest(sassDir +csopDir))
+        .pipe(rename({ suffix: ".min" }))
+        .pipe(postcss([autoprefixer(), cssnano()]))
+        .pipe(gulp.dest(csopDir));
+    //.pipe(browsersync.stream());
+}
+
+function ignoreFilecss(){
+    // var ignoreFileClearn = ignoreFile.replace('!','');
+    return gulp
+        .src(ignoreFile)
+        .pipe(plumber())
+        .pipe(sass({ outputStyle: "expanded" }))
+        .pipe(gulp.dest(csopDir));
+    //.pipe(browsersync.stream());
+}
+
+// Transpile, concatenate and minify scripts
+function scripts() {
+    return (
+        gulp
+            .src([sassDir + "js/**/*"])
+            .pipe(plumber())
+            // .pipe(webpackstream(webpackconfig, webpack))
+            // folder only, filename is specified in webpack config
+            .pipe(minify())
+            .pipe(gulp.dest(jsopDir))
+    //.pipe(browsersync.stream())
+);
+}
+
+/****** WATCH FUNCTION **************************************************************/
+
+// Watch files
+function watchFiles() {
+    gulp.watch(sassDir + "**/*", css);
+    gulp.watch(sassDir + "js/**/*", gulp.series(scriptsLint, scripts));
+    // gulp.watch(
+    //   [
+    //     "./_includes/**/*",
+    //     "./_layouts/**/*",
+    //     "./_pages/**/*",
+    //     "./_posts/**/*",
+    //     "./_projects/**/*"
+    //   ],
+    //   gulp.series(browserSyncReload)
+    // );
+    //gulp.watch("./assets/img/**/*", images);
+}
+
+/************************************************************************************/
+
+// define complex tasks
+createignoreFileList();
+const js 	= gulp.series(scripts);
+// const build = gulp.series(gulp.parallel(css,ignoreFilecss, images, js,clean));
+const build = gulp.series(gulp.parallel(css,ignoreFilecss));
+//const watch = gulp.parallel(watchFiles, browserSync);
+
+// export tasks
+exports.images 	= images;
+exports.css 	= css;
+exports.js 		= js;
+// exports.clean 	= clean;
+//exports.build 	= build;
+//exports.watch 	= watch;
+exports.default = build;
---
+++ header.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package ryanblog
+ */
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<div id="page" class="site">
+	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'ryanblog' ); ?></a>
+
+	<header id="masthead" class="site-header">
+        <div class="container">
+            <div class="row header-page-main-wrap">
+                <div class="site-branding col-sm-4">
+                    <?php
+                    the_custom_logo();
+                    $ryanblog_description = get_bloginfo( 'description', 'display' );
+                    if ( $ryanblog_description || is_customize_preview() ) :
+                        ?>
+                        <p class="site-description"><?php echo $ryanblog_description; /* WPCS: xss ok. */ ?></p>
+                    <?php endif; ?>
+                </div><!-- .site-branding -->
+                <nav id="site-navigation" class="main-navigation">
+                    <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><div class=""><span></span><span></span><span></span><span></span></div></button>
+                    <?php
+                    wp_nav_menu( array(
+                        'theme_location' => 'menu-1',
+                        'menu_id'        => 'primary-menu',
+                    ) );
+                    ?>
+                </nav><!-- #site-navigation -->
+            </div>
+        </div>
+	</header><!-- #masthead -->
+
+	<div id="content" class="site-content">
+        <div class="container">
+            <div class="row content-page-main-wrap shadow-sm">
